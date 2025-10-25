85d8c638918755e9a055936fed15db25bb6034f5
si-rui126 <65576101+si-rui126@users.noreply.github.com>
2025-10-24 21:14:56 -0700

    updated requirements.txt

--- server/create_database.py
+++ server/create_database.py
@@ -11,22 +11,36 @@ import shutil
 load_dotenv()
 openai.api_ikey = os.environ['OPENAI_API_KEY']
 
-DATA_PATH = os.path.join('processing', 'custom_md')
+DATA_PATH = os.path.join('md') # path for user uploaded data
 CHROMA_PATH = 'chroma'
 
 def main():
     generate_data_store()
+
 
 def generate_data_store():
     documents = load_documents()
     chunks = split_text(documents)
     save_to_chroma(chunks)
 
+
 def load_documents():
     text_loader_kwargs = {'autodetect_encoding': True}
     loader = DirectoryLoader(DATA_PATH, glob="*.md", loader_cls=TextLoader, loader_kwargs=text_loader_kwargs)
     documents = loader.load()
     return documents
+
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+    return chunks
 
 def save_to_chroma(chunks: list[Document]):
     # Clear out the database first.
