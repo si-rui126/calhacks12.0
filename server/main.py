2b977db33f3cb6da750d58d2391d3f281ff089bb
Llamagamer69 <gheshanshen@gmail.com>
2025-10-24 21:13:58 -0700

    monkeee

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+from nt import device_encoding
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask import send_from_directory
@@ -8,7 +9,7 @@ cors = CORS(app, origins='*')
 
 @app.route('/')
 def home():
-    return jsonify("Flask server is running on port 8080")
+    return jsonify("Flask server is running on port 8080 and i love coding")
 
 if __name__ == "__main__":
     app.run(port=8080, debug=False, use_reloader=False, threaded=False)
